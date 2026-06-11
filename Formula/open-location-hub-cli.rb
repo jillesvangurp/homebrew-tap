7d4cee100174c1b4c092fcdccad888673b89b3cd
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2026-06-11 10:01:15 +0200

    Bump open-location-hub-cli to 0.1.4

--- Formula/open-location-hub-cli.rb
+++ Formula/open-location-hub-cli.rb
@@ -3,27 +3,27 @@ class OpenLocationHubCli < Formula
   homepage "https://github.com/Open-Location-Stack/open-location-hub-cli"
 
   # STABLE-BEGIN
-  version "0.1.2"
+  version "0.1.4"
   license "MIT"
   head "https://github.com/Open-Location-Stack/open-location-hub-cli.git", branch: "main"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Open-Location-Stack/open-location-hub-cli/releases/download/0.1.2/olh-darwin-arm64-0.1.2.tar.gz"
-      sha256 "c706e3ecb58700913f55cfcae33359aeaa175ee9319de5860e3da4d019255115"
+      url "https://github.com/Open-Location-Stack/open-location-hub-cli/releases/download/0.1.4/olh-darwin-arm64-0.1.4.tar.gz"
+      sha256 "e8481018997b78442846479483cafb6fb97b14fea207a366ff96e46b52bc0294"
     else
-      url "https://github.com/Open-Location-Stack/open-location-hub-cli/releases/download/0.1.2/olh-darwin-amd64-0.1.2.tar.gz"
-      sha256 "697fb05f79a5dbf3d9f61476e898bbc2f2aac167d6b1df99c5588cea6a5271e1"
+      url "https://github.com/Open-Location-Stack/open-location-hub-cli/releases/download/0.1.4/olh-darwin-amd64-0.1.4.tar.gz"
+      sha256 "cbf17e964dede2777094e3a4cdb6ad0b5cafb8ac7073d59a35a7a9f01b0fd22e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Open-Location-Stack/open-location-hub-cli/releases/download/0.1.2/olh-linux-arm64-0.1.2.tar.gz"
-      sha256 "cec7de1ad6845e7ab6779fbb8e6886fb07d18563a530feef1545a75f17453c56"
+      url "https://github.com/Open-Location-Stack/open-location-hub-cli/releases/download/0.1.4/olh-linux-arm64-0.1.4.tar.gz"
+      sha256 "95b6721dd9aa88c4d6221781069bf1140dc0ec1340ce28e29729e7896faf4cc0"
     elsif Hardware::CPU.intel?
-      url "https://github.com/Open-Location-Stack/open-location-hub-cli/releases/download/0.1.2/olh-linux-amd64-0.1.2.tar.gz"
-      sha256 "a24a5a59bd933bed8cca744e6d11b97bc01461745d4302716969d0d7cbd70048"
+      url "https://github.com/Open-Location-Stack/open-location-hub-cli/releases/download/0.1.4/olh-linux-amd64-0.1.4.tar.gz"
+      sha256 "d237c9ff2a2125ac06e328eb234003fa2f45c6ded8043e1c94c6e1b6234d3999"
     else
       odie "open-location-hub-cli currently publishes Homebrew binaries " \
            "for macOS arm64, macOS amd64, Linux arm64, Linux amd64 only"
